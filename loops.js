81daa17b3717af9b9bbdf30e460e9f42ea9dae4a
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-14 19:36:12 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,10 @@
+function forLoop(array) {
+  for(var i=0; i<25; i++) {
+    array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`)
+  }
+  return array;
+}
+
+function whileLoop(n-- > 0) {
+  console.log(n+1);
+}
